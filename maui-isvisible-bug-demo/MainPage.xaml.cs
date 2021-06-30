310729de06d39ce383595e8ba29b4333bbe7fa89
Matt Goldman <matt.goldman@outlook.com>
2021-07-01 08:03:24 +1000

    added demo project

---
+++ maui-isvisible-bug-demo/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.Maui.Controls;
+
+namespace maui_isvisible_bug_demo
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+		bool ShowTestLabel = true;
+
+		private void OnShowLabelClicked(object sender, EventArgs e)
+		{
+			ShowTestLabel = !ShowTestLabel;
+			ShowHide.IsVisible = ShowTestLabel;
+		}
+	}
+}
---
+++ maui-isvisible-bug-demo/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+using Microsoft.Maui;
+
+namespace maui_isvisible_bug_demo
+{
+	[Register("AppDelegate")]
+	public class AppDelegate : MauiUIApplicationDelegate<Startup>
+	{
+	}
+}
